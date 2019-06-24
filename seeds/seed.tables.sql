27e2b8d5c70b57688a12d2e22779a1d9cfd3c9b9
rahmor <rahmor@yahoo.com>
2019-06-24 13:04:06 -0400

    add day 1 backend

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -5,30 +5,48 @@ TRUNCATE
   "language",
   "user";
 
+/* I moved passwords for seeding into .env file */
 INSERT INTO "user" ("id", "username", "name", "password")
 VALUES
   (
     1,
     'admin',
     'Dunder Mifflin Admin',
-    -- password = "pass"
-    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
+  ),
+  (
+    2,
+    'Nick',
+    'Nick',
+    
+  ),
+  (
+    3,
+    'Rahim',
+    'Rahim',
+
   );
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'Spanish', 1), (2, 'Spanish', 2), (3, 'Spanish', 3);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'escritorio',	'desk', 2),
+  (2, 1, 'lavaplatos',	'dishwasher', 3),
+  (3, 1, 'la iglesia',	'church', 4),
+  (4, 1, 'lapanadería',	'bakery', 5),
+  (5, 1, 'matemáticas',	'math', 6),
+  (6, 1, 'escuela',	'school', 7),
+  (7, 1, 'manta',	'blanket', 8),
+  (8, 1, 'la camisa',	'shirt', 9),
+  (9, 1, 'la cabra',	'goat', 10),
+  (10, 1, 'la charca',	'pond', 11),
+  (11, 1, 'la colina',	'hill', 12),
+  (12, 1, 'el caballo',	'horse', 13),
+  (13, 1, 'cascada',	'waterfall', 14),
+  (14, 1, 'la montaña',	'mountain', null);
+
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
